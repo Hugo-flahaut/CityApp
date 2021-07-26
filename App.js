36da0656d6574fb87e9bb906c54a3a4bf2e748c1
Hugo-flahaut <hugoflahautfeucher@gmail.com>
2021-07-26 14:20:19 +0200

    update with base project

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo}/>
+
+      <Text style={styles.title}>City App</Text>
+
+      <TouchableOpacity
+        onPress={() => alert ('Hello World')} style={styles.alert}>
+          <Text style={styles.btn}>
+            Signaler une alerte
+          </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => alert ('Hello World')} style={styles.alert}>
+          <Text style={styles.btn}>
+            Alertes en cours
+          </Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -18,4 +35,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 175,
+    marginBottom: 10 
+  },
+  title: {
+    color: '#888',
+    fontSize: 32,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  alert: {
+    backgroundColor: '#fff'
+  },
+  btn: {
+    backgroundColor: '#dfdfdf',
+    borderRadius: 10,
+    height: 50,
+    width: 320,
+    fontSize: 26,
+    color: '#fff',
+    marginHorizontal: 15,
+    marginBottom: 15,
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  }
 });
