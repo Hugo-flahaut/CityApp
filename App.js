67d1b02ed57232abc8f44a67a55a2a5fa1ded463
Hugo-flahaut <hugoflahautfeucher@gmail.com>
2021-07-26 15:34:08 +0200

    update with pick-an-image feature

--- App.js
+++ App.js
@@ -2,8 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  // Allow to use Camera and Gallery section
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("La permission d'utiliser la caméra est requise pour cet application. ");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  // if (selectedImage !== null) {
+  //   return (
+  //     <View style={styles.container}>
+  //       <Image
+  //         source={{ uri: selectedImage.localUri }}
+  //         style={styles.thumbnail}
+  //       />
+  //     </View>
+  //     );
+  //   }
+
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo}/>
@@ -22,6 +55,10 @@ export default function App() {
           <Text style={styles.btn}>
             Alertes en cours
           </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.btn}>Insérer une photo</Text>
       </TouchableOpacity>
 
     </View>
@@ -60,5 +97,10 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     textAlign: 'center',
     textAlignVertical: 'center'
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
   }
 });
